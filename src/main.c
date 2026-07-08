5b28222c2725f11ae491d67c58e57d49778e21c1
Dhruv Dhiman <dhruvdhiman905@gmail.com>
2026-07-08 14:47:37 +0530

    Made the file for main and ssd1306 and hcsr04

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "pico/stdlib.h"
+#include "hardware/timer.h"
+
+
+int main(void) {
+
+
+    return 0;
+}
