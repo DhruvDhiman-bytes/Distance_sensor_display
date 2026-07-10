71618a308d044598b6d8e05d206dc5c747a87fa2
Dhruv Dhiman <dhruvdhiman905@gmail.com>
2026-07-10 15:50:19 +0530

    Added the macro for EXT_SUCCESS and EXT_FAILURE and function of stdio_init_all()

--- src/main.c
+++ src/main.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
+#include "pico/binary_info.h"
 #include "pico/stdlib.h"
 #include "hardware/timer.h"
 
+// ========= space for macros and global variables =====================
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+// ========= space for user defined functions ==========================
+
+
 
 int main(void) {
-
+    stdio_init_all();
 
     return 0;
 }
